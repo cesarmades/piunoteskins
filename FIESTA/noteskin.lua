20f754b9866d19ecd96737d040baa49f6649dadd
cesarmades <65831376+cesarmades@users.noreply.github.com>
2020-05-23 23:41:09 -0500

    File deletion
    Deleting extra noteskins just to focus on the main one.

--- FIESTA/noteskin.lua
+++
@@ -1,9 +0,0 @@
-return {
-	notes= "notes.lua",
-	layers= {"receptors.lua", "explosions.lua"},
-	supports_all_buttons= false,
-	buttons= {"UpLeft", "UpRight", "DownLeft", "DownRight", "Center"},
-	fallback= "",
-	skin_parameters= {},
-	skin_parameter_info= {},
-}
